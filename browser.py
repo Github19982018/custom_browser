c199cf469ada8db546bbbaa32d1c9a8b1b87f211
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-10-26 22:00:10 +0530

    added gui

--- browser.py
+++ browser.py
@@ -103,15 +103,31 @@ def show(body):
         content = content.replace('&gt;','>').replace('&lt;','<')
     print(content)
               
-def load(url):
-    body = url.request()
-    if url.view_source:
-        print(body)
-    else:
-        show(body)
+import tkinter
+WIDTH, HEIGHT = 800, 600
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window, 
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+        
+    def load(self, url):
+        body = url.request()
+        if url.view_source:
+            print(body)
+        else:
+            show(body)
+        self.canvas.create_rectangle(10, 29, 400, 300)
+        self.canvas.create_oval(100, 100, 150, 150)
+        self.canvas.create_text(200, 150, text="Hi!")
 
 if __name__ == '__main__':
     import sys
-    load(URL(sys.argv[1]))
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
     
-        
