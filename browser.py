661ee202c68e73576f5efa07bb95cc0dd94c9c7c
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-10-26 14:46:04 +0530

    added show function

--- browser.py
+++ browser.py
@@ -33,4 +33,16 @@ class URL:
         content = response.read()
         s.close()
         return content
+    
+    
+def show(body):
+    in_tag = False
+    for c in body:
+        if c == '<':
+            in_tag = True
+        elif c == '>':
+            in_tag = False
+        elif not in_tag:
+            print(c, end='')
+            
         
