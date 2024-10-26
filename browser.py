da61e20a68090c7b079be9137d957df63f8062ae
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-10-26 21:38:08 +0530

    added support data scheme

--- browser.py
+++ browser.py
@@ -15,7 +15,7 @@ class URL:
             self.port = 80
         elif self.scheme == 'https':
             self.port = 443
-        elif self.scheme =='file' and not url.startswith('/'):
+        elif self.scheme in ['data','file'] and not url.startswith('/'):
             url = '/' + url 
         if '/' not in url:
             url = url + '/'
@@ -28,61 +28,67 @@ class URL:
     def request(self):
         if self.scheme == 'file':
             path = self.path.strip('/')
+            self.type = path.rsplit('.', 1)
             with open(path,'r') as response:
                 content = response.read()
                 return content
-        s = socket.socket(
-            family = socket.AF_INET, 
-            type = socket.SOCK_STREAM,
-            proto = socket.IPPROTO_TCP,
-        )
-        
-        if self.scheme == 'https':
-            ctx = ssl.create_default_context()
-            s =ctx.wrap_socket(s, server_hostname=self.host)
-        s.connect((self.host, self.port))
-        request = 'GET {} HTTP/1.0\r\n'.format(self.path)
-        request += 'Host: {}\r\n'.format(self.host)
-        # request += 'Connection: close\r\n'
-        request += 'User-Agent: test\r\n'
-        request += 'Accept-Encoding: gzip\r\n'
-        request += '\r\n'
-        s.send(request.encode('utf8'))
-        response = s.makefile('r', encoding='utf8', newline='\r\n')
-        statusline = response.readline()
-        version, status, explanation = statusline.split(' ', 2)
-        if 300 <= int(status) < 400:
-            if self.__class__.redirect > 3:
-                return 'too many redirects'
-            self.__class__.redirect += 1
-            url = response_headers['location']
-            if url.startswith('/'):
-                return self.request()
-            else:
-                self.__init__(url)
-                return self.request()
-        response_headers = {}
-        while True:
-            line = response.readline()
-            if line == '\r\n': break 
-            header, value = line.split(':', 1)
-            response_headers[header.casefold()] = value.strip()
-        if response_headers.get('transfer-encoding','') == 'chunked':
+        elif self.scheme == 'data': 
+            content = self.path.strip('/')
+            self.type, content = content.split(',', 1)
+            return content
+        else:    
+            s = socket.socket(
+                family = socket.AF_INET, 
+                type = socket.SOCK_STREAM,
+                proto = socket.IPPROTO_TCP,
+            )
+            
+            if self.scheme == 'https':
+                ctx = ssl.create_default_context()
+                s =ctx.wrap_socket(s, server_hostname=self.host)
+            s.connect((self.host, self.port))
+            request = 'GET {} HTTP/1.0\r\n'.format(self.path)
+            request += 'Host: {}\r\n'.format(self.host)
+            # request += 'Connection: close\r\n'
+            request += 'User-Agent: test\r\n'
+            request += 'Accept-Encoding: gzip\r\n'
+            request += '\r\n'
+            s.send(request.encode('utf8'))
+            response = s.makefile('r', encoding='utf8', newline='\r\n')
+            statusline = response.readline()
+            version, status, explanation = statusline.split(' ', 2)
+            if 300 <= int(status) < 400:
+                if self.__class__.redirect > 3:
+                    return 'too many redirects'
+                self.__class__.redirect += 1
+                url = response_headers['location']
+                if url.startswith('/'):
+                    return self.request()
+                else:
+                    self.__init__(url)
+                    return self.request()
+            response_headers = {}
             while True:
-                size = response.readline()
-                if int(size) == 0:
-                    break
-                content += response.readline(int(size))
+                line = response.readline()
+                if line == '\r\n': break 
+                header, value = line.split(':', 1)
+                response_headers[header.casefold()] = value.strip()
+            if response_headers.get('transfer-encoding','') == 'chunked':
+                while True:
+                    size = response.readline()
+                    if int(size) == 0:
+                        break
+                    content += response.readline(int(size))
+                return content
+            if response_headers.get('content-encoding','') == 'gzip':
+                response = s.makefile('rb', newline='\r\n')
+                content = gzip.decompress(response.read())
+                return content
+            size = response_headers['content-length']
+            content = response.read(int(size))
+            __class__.socket = s
             return content
-        if response_headers.get('content-encoding','') == 'gzip':
-            response = s.makefile('rb', newline='\r\n')
-            content = gzip.decompress(response.read())
-            return content
-        size = response_headers['content-length']
-        content = response.read(int(size))
-        __class__.socket = s
-        return content
-
+    
     
 def show(body):
     in_tag = False
